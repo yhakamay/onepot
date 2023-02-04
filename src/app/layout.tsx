c41b86a498915db9dbed97bd84e8fd1bf5cc6f40
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-02-05 01:23:29 +0900

    set up ChakraProvider
    Chakra components are client components, so declaring MyChakraProvider with 'use client'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import MyChakraProvider from "./my_chakra_provider";
+
 export default function RootLayout({
   children,
 }: {
@@ -6,7 +8,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body>
+        <MyChakraProvider>{children}</MyChakraProvider>
+      </body>
     </html>
   );
 }
