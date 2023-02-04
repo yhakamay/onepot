8c186ca589bb0a07ce89cdeafb57d43abd86842f
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-02-05 01:59:17 +0900

    add Header

---
+++ src/app/components/organisms/header.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Box, Container, Flex, Heading } from "@chakra-ui/react";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <Box as="section" pb={{ base: "4", md: "8" }}>
+      <Box as="nav" bg="bg-surface" boxShadow="sm">
+        <Container py={{ base: "3", lg: "4" }}>
+          <Link href="/">
+            <Heading size="lg">OnePot</Heading>
+          </Link>
+        </Container>
+      </Box>
+    </Box>
+  );
+}
