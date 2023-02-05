13d9e0eb05b86140cbfd93ce448c2a631f0fe348
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-02-05 13:34:33 +0900

    create page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,77 @@
-import { Inter } from "@next/font/google";
+"use client";
 
-const inter = Inter({ subsets: ["latin"] });
+import Image from "next/image";
+import {
+  Box,
+  Button,
+  Heading,
+  Select,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  Text,
+  VStack,
+} from "./components/atoms/my_chakra_components";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import { useState } from "react";
 
 export default function Home() {
-  return <p>AAA</p>;
+  const [time, setTime] = useState(30);
+  const onTimeChange = (value: number) => setTime(value);
+  const [type, setType] = useState("");
+  const onTypeChange = (value: string) => setType(value);
+
+  return (
+    <>
+      <VStack spacing="4">
+        <Box h={16} />
+        <Heading
+          fontWeight="black"
+          textAlign="center"
+          size="3xl"
+        >{`Ask AI what should you cook today.`}</Heading>
+        <Image src="/cooking.svg" width={200} height={200} alt={"cooking"} />
+        <Box h={4} />
+        <Text
+          alignSelf="start"
+          fontSize="lg"
+        >{`❶ How long can you take for cooking?`}</Text>
+        <Slider
+          min={10}
+          max={60}
+          step={10}
+          value={time}
+          onChange={onTimeChange}
+        >
+          <SliderTrack>
+            <SliderFilledTrack />
+          </SliderTrack>
+          <SliderThumb fontSize="sm" boxSize="32px">
+            {time}
+          </SliderThumb>
+        </Slider>
+        <Text alignSelf="end" fontSize="sm" color="grey">
+          min
+        </Text>
+        <Box h={4} />
+        <Text alignSelf="start" fontSize="lg">{`❷ Which meal?`}</Text>
+        <Select
+          placeholder="Select type"
+          onChange={(e) => onTypeChange(e.target.value)}
+        >
+          <option value="breakfast">Breakfast</option>
+          <option value="lunch">Lunch</option>
+          <option value="dinner">Dinner</option>
+        </Select>
+        <Button
+          w="full"
+          rightIcon={<BsFillArrowRightCircleFill />}
+          isDisabled={time === 0 || type === ""}
+        >
+          Ask AI
+        </Button>
+      </VStack>
+    </>
+  );
 }
