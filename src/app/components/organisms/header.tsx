900b0a517978b049a99904551088e917743c5610
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-02-05 13:27:10 +0900

    add a link to GitHub repo on Header

--- src/app/components/organisms/header.tsx
+++ src/app/components/organisms/header.tsx
@@ -1,14 +1,34 @@
 import Link from "next/link";
-import { Box, Container, Heading } from "../atoms/my_chakra_components";
+import { BsGithub } from "react-icons/bs";
+import {
+  Box,
+  Container,
+  Heading,
+  HStack,
+  IconButton,
+} from "../atoms/my_chakra_components";
 
 export default function Header() {
   return (
     <Box as="section" pb={{ base: "4", md: "8" }}>
       <Box as="nav" bg="bg-surface" boxShadow="sm">
         <Container py={{ base: "3", lg: "4" }}>
-          <Link href="/">
-            <Heading size="lg">OnePot</Heading>
-          </Link>
+          <HStack justify="space-between">
+            <Link href="/">
+              <Heading size="lg">OnePot</Heading>
+            </Link>
+            <Link
+              href={"https://github.com/yhakamay/onepot"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconButton
+                variant="link"
+                icon={<BsGithub />}
+                aria-label={"github"}
+              />
+            </Link>
+          </HStack>
         </Container>
       </Box>
     </Box>
